c0a6122e5d8d83e259495fc5e94640f0218431ee
HarshPanjwani <panjwani28.harsh@gmail.com>
2018-03-25 17:29:40 +0530

    TClogin

--- app/src/main/java/com/siaans/skillindia/fragment/CourseFragment.java
+++ app/src/main/java/com/siaans/skillindia/fragment/CourseFragment.java
@@ -103,8 +103,20 @@ public class CourseFragment extends Fragment {
         listDataHeader.add("Agriculture Sector Skill Council");
         listDataHeader.add("Construction");
         listDataHeader.add("Automotive");
-
+        listDataHeader.add("Beauty and Wellness");
+        listDataHeader.add("Capital Goods");
+        listDataHeader.add("Domestic Workers");
+        listDataHeader.add("Earthmoving & Infrastructure Building");
+        listDataHeader.add("Electronics & Hardware");
+        listDataHeader.add("Food Processing");
+        listDataHeader.add("Furniture and Fitting");
+        listDataHeader.add("Gems and Jewellery");
+        listDataHeader.add("Green Jobs");
+        listDataHeader.add("Handicrafts");
+        listDataHeader.add("Healthcare");
+        listDataHeader.add("Iron and Steel");
         // Adding child data
+
         List<String> Agriculture = new ArrayList<String>();
         Agriculture.add("Animal Health Worker");
         Agriculture.add("Aquaculture Worker");
@@ -134,9 +146,107 @@ public class CourseFragment extends Fragment {
         Automotive.add("Dealership Telecaller Sales Executive");
         Automotive.add("Showroom Hostess Customer Relationship Executive");
 
+        List<String> Beauty = new ArrayList<String>();
+        Beauty.add("Assistant Beauty Therapist");
+        Beauty.add("Assistant Hair Stylist");
+        Beauty.add("Assistant Nail Technician");
+        Beauty.add("Assistant Spa Therapist");
+        Beauty.add("Beauty Therapist");
+
+        List<String> Capital = new ArrayList<String>();
+        Capital.add("CNC Operator Turning");
+        Capital.add("Assistant MMAW-SMAW Welder");
+        Capital.add("Assistant Oxy Fuel-Gas Cutter");
+        Capital.add("Draughtsman – Mechanical");
+        Capital.add("Fitter – Electrical and Electronic Assembly");
+
+        List<String> Domestic = new ArrayList<String>();
+        Domestic.add("Child Caretaker");
+        Domestic.add("Elderly Caretaker (Non-Clinical)");
+        Domestic.add("General Housekeeper");
+        Domestic.add("Housekeeper cum cook");
+
+        List<String> Earthmoving = new ArrayList<String>();
+        Earthmoving.add("Backhoe Loader Operator");
+        Earthmoving.add("Batching Plant Operator");
+        Earthmoving.add("Compactor Operator");
+        Earthmoving.add("Compressor Operator");
+        Earthmoving.add("Concrete Pump Operator");
+
+        List<String> Electronics = new ArrayList<String>();
+        Electronics.add("CCTV Installation Technician");
+        Electronics.add("DTH Set Top Box Installation & Service Technician");
+        Electronics.add("Field Technician – Computing and Peripherals");
+        Electronics.add("Field Technician – Networking and Storage");
+        Electronics.add("Field Technician – Other Home Appliances");
+
+        List<String> Food = new ArrayList<String>();
+        Food.add("Baking Technician");
+        Food.add("Craft Baker");
+        Food.add("Jam Jelly & Ketchup Processing Technician");
+        Food.add("Pickle Making Technician");
+        Food.add("Plant Biscuit Production Specialist");
+
+        List<String> Furniture = new ArrayList<String>();
+        Furniture.add("Assistant Carpenter Wooden Furniture");
+        Furniture.add("Carpenter - Wooden Furniture");
+        Furniture.add("Assistant Fitter Modular Furniture");
+        Furniture.add("Fitter Modular Furniture");
+
+        List<String> Gems = new ArrayList<String>();
+        Gems.add("Cast and diamonds-set jewellery - CAD Operator");
+        Gems.add("Cast and diamonds-set jewellery - Hand Sketch Designer (Basic)");
+        Gems.add("Cast & Diamonds Set Jewellery - Wax Setter");
+        Gems.add("Cast & diamonds-set jewellery Merchandiser Design");
+        Gems.add("Cast & diamonds-set jewellery Metal Setter(Basic)");
+
+        List<String> Green = new ArrayList<String>();
+        Green.add("Solar PV Installer - Civil");
+        Green.add("Solar PV Installer - Electrical");
+        Green.add("Solar PV Installer (Suryamitra)");
+        Green.add("Wastewater Treatment Plant Helper");
+        Green.add("Wastewater Treatment Plant Technician");
+
+        List<String> Handicrafts = new ArrayList<String>();
+        Handicrafts.add("Agarbatti Packer");
+        Handicrafts.add("Bamboo Basket Maker");
+        Handicrafts.add("Bamboo Mat Weaver");
+        Handicrafts.add("Bamboo Utility Handicraft Assembler");
+        Handicrafts.add("Hammering Artisan");
+
+        List<String> Healthcare = new ArrayList<String>();
+        Healthcare.add("Diabetes Educator");
+        Healthcare.add("Diet Assistant");
+        Healthcare.add("Emergency Medical Technician-Basic");
+        Healthcare.add("Frontline Health Worker");
+        Healthcare.add("General Duty Assistant");
+
+        List<String> Iron = new ArrayList<String>();
+        Iron.add("Bearing Maintenance");
+        Iron.add("EOT Overhead Crane Operator");
+        Iron.add("Fitter Electrical Assembly");
+        Iron.add("Fitter Electronic Assembly");
+        Iron.add("Fitter Instrumentation");
+
+
+
+
         listDataChild.put(listDataHeader.get(0), Agriculture); // Header, Child data
         listDataChild.put(listDataHeader.get(1), Construction);
         listDataChild.put(listDataHeader.get(2), Automotive);
+        listDataChild.put(listDataHeader.get(3), Beauty);
+        listDataChild.put(listDataHeader.get(4), Capital);
+        listDataChild.put(listDataHeader.get(5), Domestic);
+        listDataChild.put(listDataHeader.get(6), Earthmoving);
+        listDataChild.put(listDataHeader.get(7), Electronics);
+        listDataChild.put(listDataHeader.get(8), Food);
+        listDataChild.put(listDataHeader.get(9), Furniture);
+        listDataChild.put(listDataHeader.get(10), Gems);
+        listDataChild.put(listDataHeader.get(11), Green);
+        listDataChild.put(listDataHeader.get(12), Handicrafts);
+        listDataChild.put(listDataHeader.get(13), Healthcare);
+        listDataChild.put(listDataHeader.get(14), Iron);
+
     }
 
 
